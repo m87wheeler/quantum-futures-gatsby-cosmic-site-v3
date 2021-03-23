b06a39c63824514e2b9f6380dd1c0fc4b15397ea
m87wheeler <m87.wheeler@gmail.com>
2021-03-23 15:40:35 +0000

    Update Footer.js

--- src/components/views/Footer/Footer.js
+++ src/components/views/Footer/Footer.js
@@ -10,6 +10,7 @@ import Typography from "../../../components/single/Typography/Typography";
 import SocialMediaIcons from "../../composite/SocialMediaIcons/SocialMediaIcons";
 
 // *** styled components
+const List = styled.ul``;
 const Wrapper = styled.footer`
   display: grid;
   grid-template-rows: auto auto;
@@ -19,25 +20,42 @@ const Wrapper = styled.footer`
   background: ${(p) => `rgba(${p.theme.common.black}, .9)`};
   color: ${(p) => `rgb(${p.theme.common.white})`};
 
-  ul {
+  li {
+    list-style-type: none;
+  }
+
+  ${List} {
     display: flex;
     flex-flow: column nowrap;
     align-items: center;
-    list-style: none;
   }
 
   @media (min-width: ${(p) => p.theme.media.md.min}) {
     display: grid;
     grid-template-columns: repeat(12, 1fr);
     gap: 1rem;
+
+    li {
+      align-self: center;
+    }
+
+    ${List} {
+      grid-column: 7 / 11;
+    }
+  }
+`;
+
+const StyledSocialIcons = styled(SocialMediaIcons)`
+  @media (min-width: ${(p) => p.theme.media.md.min}) {
+    grid-column: 2 / 6;
   }
 `;
 
 const Footer = ({ ...props }) => {
   return (
     <Wrapper {...props}>
-      <SocialMediaIcons />
-      <ul>
+      <StyledSocialIcons />
+      <List>
         <li>
           <Typography size="sm">
             <Link to="/sitemap.xml">Sitemap</Link>
@@ -56,7 +74,7 @@ const Footer = ({ ...props }) => {
             </Link>
           </Typography>
         </li>
-      </ul>
+      </List>
     </Wrapper>
   );
 };
