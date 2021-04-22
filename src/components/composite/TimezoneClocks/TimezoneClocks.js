ef20eda4df99c6143c533725d720c4f4e2391fa5
Martin <m87.wheeler@gmail.com>
2021-04-22 13:21:17 +0100

    Removed TimezoneClocks.style
    Affecting Gatsby cloud build - checking error

--- src/components/composite/TimezoneClocks/TimezoneClocks.js
+++ src/components/composite/TimezoneClocks/TimezoneClocks.js
@@ -1,4 +1,5 @@
 import * as React from "react";
+import styled from "styled-components";
 
 // *** data, hooks & context
 
@@ -8,7 +9,36 @@ import Clock from "../../single/Clock/Clock";
 import Typography from "../../single/Typography/Typography";
 
 // *** styled components
-import { Wrapper, ClockContainer } from "./TimezoneClocks.style";
+// import { Wrapper, ClockContainer } from "./TimezoneClocks.style";
+export const ClockContainer = styled.div`
+  display: grid;
+  grid-template-rows: auto auto 1fr auto;
+  gap: 1rem;
+  align-content: flex-end;
+
+  @media (min-width: ${(p) => p.theme.media.sm.min}) {
+    ${ClockContainer} {
+      justify-self: ${(p) =>
+        p.index === 0
+          ? "flex-start"
+          : p.index === p.lastItem
+          ? "flex-end"
+          : "center"};
+    }
+  }
+`;
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-flow: column nowrap;
+  align-items: center;
+
+  @media (min-width: ${(p) => p.theme.media.sm.min}) {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    align-items: flex-start;
+  }
+`;
 
 const TimezoneClocks = ({ timezoneArr, ...props }) => {
   return (
--- src/components/composite/TimezoneClocks/TimezoneClocks.style.js
+++ src/components/composite/TimezoneClocks/TimezoneClocks.style.js
@@ -1,31 +1,31 @@
-import styled from "styled-components";
+// import styled from "styled-components";
 
-export const ClockContainer = styled.div`
-  display: grid;
-  grid-template-rows: auto auto 1fr auto;
-  gap: 1rem;
-  align-content: flex-end;
+// export const ClockContainer = styled.div`
+//   display: grid;
+//   grid-template-rows: auto auto 1fr auto;
+//   gap: 1rem;
+//   align-content: flex-end;
 
-  @media (min-width: ${(p) => p.theme.media.sm.min}) {
-    ${ClockContainer} {
-      justify-self: ${(p) =>
-        p.index === 0
-          ? "flex-start"
-          : p.index === p.lastItem
-          ? "flex-end"
-          : "center"};
-    }
-  }
-`;
+//   @media (min-width: ${(p) => p.theme.media.sm.min}) {
+//     ${ClockContainer} {
+//       justify-self: ${(p) =>
+//         p.index === 0
+//           ? "flex-start"
+//           : p.index === p.lastItem
+//           ? "flex-end"
+//           : "center"};
+//     }
+//   }
+// `;
 
-export const Wrapper = styled.div`
-  display: flex;
-  flex-flow: column nowrap;
-  align-items: center;
+// export const Wrapper = styled.div`
+//   display: flex;
+//   flex-flow: column nowrap;
+//   align-items: center;
 
-  @media (min-width: ${(p) => p.theme.media.sm.min}) {
-    display: grid;
-    grid-template-columns: repeat(3, 1fr);
-    align-items: flex-start;
-  }
-`;
+//   @media (min-width: ${(p) => p.theme.media.sm.min}) {
+//     display: grid;
+//     grid-template-columns: repeat(3, 1fr);
+//     align-items: flex-start;
+//   }
+// `;
