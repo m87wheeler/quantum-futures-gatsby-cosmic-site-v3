c4265684705db0cf0e3b4404dfc8fd8ae0b2bc24
Martin <m87.wheeler@gmail.com>
2021-04-27 10:22:26 +0100

    Update Contact.style.js
    Change layout

--- src/style/pages/Contact.style.js
+++ src/style/pages/Contact.style.js
@@ -30,7 +30,7 @@ export const IntroWrapper = styled(LandingWrapper)`
   min-height: 0;
 
   @media (min-width: ${(p) => p.theme.media.md.min}) {
-    grid-column: 1 / span 8;
+    grid-column: 1 / span 12;
   }
   ${fadeInAnimation};
 `;
@@ -49,10 +49,9 @@ export const SocialWrapper = styled(LandingWrapper)`
   padding-top: 1rem;
 
   @media (min-width: ${(p) => p.theme.media.md.min}) {
-    grid-row: 1 / 2;
+    grid-row: 2 / 3;
     grid-column: 9 / span 4;
     padding-left: 0;
-    padding-top: 7.5rem;
   }
   ${fadeInAnimation};
 `;
@@ -63,9 +62,9 @@ export const MapWrapper = styled(LandingWrapper)`
 
   @media (min-width: ${(p) => p.theme.media.md.min}) {
     height: 100%;
-    padding-left: 0;
-    grid-row: 2 / 3;
-    grid-column: 9 / span 4;
+    padding: 1rem 0;
+    grid-row: 3 / 4;
+    grid-column: 1 / span 12;
   }
   ${fadeInAnimation};
 `;
@@ -90,3 +89,96 @@ export const OfficesWrapper = styled(LandingWrapper)`
   }
   ${fadeInAnimation};
 `;
+
+// import styled, { css } from "styled-components";
+// import { LandingWrapper } from "./About.style";
+
+// export const appearDuration = 500;
+// export const classes = {
+//   hero: "hero",
+// };
+
+// const fadeInAnimation = css`
+//   &.${classes.hero}-appear {
+//     opacity: 0;
+//     margin-top: 3rem;
+//   }
+
+//   &.${classes.hero}-enter {
+//     opacity: 0;
+//     margin-top: 3rem;
+//   }
+
+//   &.${classes.hero}-enter-done {
+//     opacity: 1;
+//     margin-top: 0rem;
+//     transition: opacity ${appearDuration}ms ease-out,
+//       margin-top ${appearDuration}ms ease-in-out;
+//     transition-delay: ${(p) => `${p.delay * 250 + 250}ms`};
+//   }
+// `;
+
+// export const IntroWrapper = styled(LandingWrapper)`
+//   min-height: 0;
+
+//   @media (min-width: ${(p) => p.theme.media.md.min}) {
+//     grid-column: 1 / span 8;
+//   }
+//   ${fadeInAnimation};
+// `;
+
+// export const FormWrapper = styled(LandingWrapper)`
+//   padding-top: 1rem;
+
+//   @media (min-width: ${(p) => p.theme.media.md.min}) {
+//     grid-column: 1 / span 8;
+//   }
+//   ${fadeInAnimation};
+// `;
+
+// export const SocialWrapper = styled(LandingWrapper)`
+//   min-height: 0;
+//   padding-top: 1rem;
+
+//   @media (min-width: ${(p) => p.theme.media.md.min}) {
+//     grid-row: 1 / 2;
+//     grid-column: 9 / span 4;
+//     padding-left: 0;
+//     padding-top: 7.5rem;
+//   }
+//   ${fadeInAnimation};
+// `;
+
+// export const MapWrapper = styled(LandingWrapper)`
+//   min-height: 0;
+//   padding-top: 1rem;
+
+//   @media (min-width: ${(p) => p.theme.media.md.min}) {
+//     height: 100%;
+//     padding-left: 0;
+//     grid-row: 2 / 3;
+//     grid-column: 9 / span 4;
+//   }
+//   ${fadeInAnimation};
+// `;
+
+// // TODO remove TempMap
+// export const TempMap = styled.div`
+//   width: 100%;
+//   height: 16rem;
+//   background: #aaa;
+
+//   @media (min-width: ${(p) => p.theme.media.md.min}) {
+//     height: 100%;
+//   }
+//   ${fadeInAnimation};
+// `;
+
+// export const OfficesWrapper = styled(LandingWrapper)`
+//   padding-top: 1rem;
+
+//   @media (min-width: ${(p) => p.theme.media.md.min}) {
+//     grid-column: 1 / span 12;
+//   }
+//   ${fadeInAnimation};
+// `;
