51c86b338b946b30c02a07cc71c4082788c0e9a5
Martin <m87.wheeler@gmail.com>
2021-05-26 20:25:48 +0100

    Update Layout.js

--- src/style/Layout.js
+++ src/style/Layout.js
@@ -37,10 +37,10 @@ const Layout = ({ hideLogo, background, backdropActive, children }) => {
     <Theme>
       <Helmet>
         <link rel="icon" href={favicon} />
-        <meta
+        {/* <meta
           http-equiv="refresh"
           content="0;url=https://quantumfutures.gatsbyjs.io/under-construction/"
-        />
+        /> */}
       </Helmet>
       <GlobalStyle />
       <StyledHeader backdropActive={backdropActive} hideLogo={hideLogo} />
